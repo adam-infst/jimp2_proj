9afd5659ac5b36467833105dc5434835a7b9c646
Live session user <01197805@pw.edu.pl>
2025-03-05 23:56:21 +0000

    losowanie

--- src/main.c
+++ src/main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-const int max=5;
+#define max 5
 
 int main() {
 
 	int n = 0;
-	char *c = NULL;
-
-
-	c = malloc(max*sizeof(char));
+	char c[max];
+	//char* c = malloc((max+1)*sizeof(char));
 	printf("podaj liczbe wierzcholkow: ");
 	fgets(c, max, stdin);
+	printf("work");
 	if(c == NULL){
 		return 1;
 	}
 	n = atoi(c);
-        int (*T)[n] = malloc(sizeof(int[n][n]));
+	printf("%d",n);
+	int **T = malloc(sizeof(int[n][n]));
 	if(T == NULL){
 		return 2;
 	}
@@ -28,8 +29,19 @@ int main() {
 		}
 	}
 	int a = 0, b = 0;
+	int r=n*n*2+1;
+	srand(time(NULL));
+	char *los;
+	printf("jesli chcesz by graf byl losowany, wpisz y, by w przeciwnym wypadku kliknij enter");
+	fgets(los,1,stdin);
 	for(;;)
 	{
+		if(los[0] == 'y')
+		{
+		a=rand()%n+1;
+		b=rand()%n+1;
+		}else
+		{
 		a = 0;
 		b = 0;
 		printf("podaj wierzcholek z ktorego chcesz zrobic polaczenie, lub q jesli chcesz przestac pisac wierzcholki:");
@@ -42,12 +54,22 @@ int main() {
 		printf("podaj wierzcholek do ktorego chcesz zrobic polaczenie:");
 		fgets(c, max, stdin);
 		b = atoi(c);
-		if(a>0 && b>0 && a!=b && a<=n && b<=n)
+		}
+		if(los[0] == 'y'  &&  rand()%r == 0 || c[0]=='q')
+		{
+			break;
+		}
+		if(a>0 && b>0 && a<=n && b<=n)
 		{
 			T[a-1][b-1]=1;
 		}
 	}
 	printf("wierzcholki w grafie: ");
+	for(int i = 0; i < n; i++)
+	{
+		printf("W%d ",i);
+	}
+	printf("\npolaczenia w grafie:");
 	for(int i = 0; i < n; i++)
 	{
 		printf("\n");
@@ -59,5 +81,7 @@ int main() {
 		}
 	}
 	printf("\n");
+	//free(c);
+	free(T);
 	return 0;
 }
